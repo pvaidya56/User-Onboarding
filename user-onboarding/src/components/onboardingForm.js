0a55d264167e58fcdcfa15f81672a305bc96e941
Priya Vaidya <pvaidya56@gmail.com>
2019-08-19 17:32:14 -0700

    reached mvp

--- user-onboarding/src/components/onboardingForm.js
+++ user-onboarding/src/components/onboardingForm.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
 
- const OnboardingForm = ({errors, touched}) => {
+ const OnboardingForm = ({errors, touched, values, status}) => {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    if(status) {
+      setUsers([...users, status])
+    }
+  }, [status])
+
   return (
     <div className="onboarding-form">
       <Form>
@@ -24,14 +31,17 @@ import axios from 'axios'
           {touched.email && errors.email && <p className="error">{errors.email}</p>}
           <Field 
             component="input"
-            type="text"
+            type="password"
             name="password"
             placeholder="Password"
+            htmlFor="password"
           />
+           {touched.password && errors.password && <p className="error">{errors.password}</p>}
           <label className="tos">I have read and accept the Terms of Service
             <Field 
               type="checkbox"
               name="tos"
+              checked={values.tos}
             />
             {touched.tos && errors.tos && <p className="error">{errors.tos}</p>}
             <span className="checkmark" />
@@ -39,6 +49,13 @@ import axios from 'axios'
           <button>Submit!</button>
         </div>
       </Form>  
+      {users.map(user => {
+        return (
+          <div>
+            <p key={user.id}><span>Welcome, </span>{user.name}</p>
+            <p key={user.id}><span>You are logged in with email: </span>{user.email}</p>
+          </div>  
+      )})}
     </div>
   )
 }
@@ -56,9 +73,17 @@ import axios from 'axios'
     name: Yup.string().required('Enter your Name'),
     email: Yup.string().email(' ').required('Enter your Email'),
     password: Yup.string().required('Enter your password'),
-    tos: Yup.boolean().required('Agree to proceed')
+    tos: Yup.boolean().oneOf([true], 'Please read and agree to the terms of service to proceed')
+	  }),
+
+   handleSubmit(values, { setStatus }) {
+    axios
+      .post('https://reqres.in/api/users', values)
+      .then(res => setStatus(res.data))
+      .catch(err => console.error(err))
+  }
   })
-})
+
 
  const FormikOnboardingForm = formikHOC(OnboardingForm)
 
